8b503307d7321155d49dea2a8f0270c84f53ce4f
Ippei Mukaida <mr.sunege@gmail.com>
2026-05-05 18:13:08 +0900

    fix(sample): remove unused imports in ChatInputSample

--- sample-app/src/main/java/dev/mkeeda/arranger/sampleApp/ChatInputSample.kt
+++ sample-app/src/main/java/dev/mkeeda/arranger/sampleApp/ChatInputSample.kt
@@ -3,11 +3,9 @@ package dev.mkeeda.arranger.sampleApp
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
