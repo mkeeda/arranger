c405be9ab143e4cc3d8c7263b0c4116b4fd20b6f
Ippei Mukaida <mr.sunege@gmail.com>
2026-05-25 18:35:56 +0900

    clean: remove redundant OptIn annotations

--- richtext/src/commonMain/kotlin/dev/mkeeda/arranger/richtext/RichString.kt
+++ richtext/src/commonMain/kotlin/dev/mkeeda/arranger/richtext/RichString.kt
@@ -87,7 +87,6 @@ public data class RichString(
      * Executes the given [block] on a [RichStringScope] scoped to this [RichString],
      * and returns a completely new [RichString] carrying the modifications.
      */
-    @OptIn(InternalArrangerApi::class)
     public fun edit(block: RichStringScope.() -> Unit): RichString {
         val scope = RichStringScope(currentSpans = spans, text = text)
         scope.block()
