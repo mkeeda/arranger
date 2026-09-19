7aafc94f1b8f856703376606874c98638c492abd
Ippei Mukaida <mr.sunege@gmail.com>
2026-09-19 16:18:50 +0900

    feat(dokka): configure multi-module Dokka HTML API reference generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,26 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.dokka) apply false
+}
+
+allprojects {
+    extra["org.jetbrains.dokka.experimental.gradle.pluginMode"] = "V2Enabled"
+}
+
+apply(plugin = "org.jetbrains.dokka")
+
+dependencies {
+    "dokka"(project(":richtext"))
+    "dokka"(project(":richtext-editor"))
+    "dokka"(project(":richtext-editor-material3"))
+    "dokka"(project(":richtext-markdown"))
+    "dokka"(project(":richtext-html"))
+}
+
+tasks.register("dokkaHtmlMultiModule") {
+    dependsOn(tasks.named("dokkaGeneratePublicationHtml"))
+    description = "Generates multi-module HTML documentation using Dokka"
+    group = "documentation"
 }
